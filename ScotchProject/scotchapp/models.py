e5e2d74b6fd5729e5220c5175413789c8b131e25
Solo Coder <kisliydanecka@gmail.com>
2023-11-19 22:12:38 +0300

    Database

--- ScotchProject/scotchapp/models.py
+++ ScotchProject/scotchapp/models.py
@@ -2,29 +2,29 @@ from django.db import models
 
 
 # модель категорий в админке, вводим их наименования
-class Category(models.Model):
-    name = models.CharField('Категория', max_length=100)
+class MenuItem(models.Model):
+    image = models.ImageField(verbose_name='Изображение', upload_to='images')
+    title = models.CharField(max_length=80, verbose_name='Название')
+    weight = models.CharField(max_length=15, verbose_name='Вес')
+    price = models.DecimalField(max_digits=10, decimal_places=0, verbose_name='Цена')
+    description = models.TextField(max_length=120, default=0, verbose_name='Состав')
+
+    TYPE = [
+        ('eat', 'Закуски'),
+        ('soup', 'Супы'),
+        ('pizza', 'Пицца'),
+        ('pasta', 'Паста'),
+        ('sandwitch', 'Сэндвичи'),
+        ('hot', 'Горячие блюда'),
+        ('desert', 'Десерты'),
+        ('drinks', 'Напитки'),
+        ('sous', 'Соусы'),
+        ('salat', 'Салаты')
+    ]
+
+    type = models.CharField(choices=TYPE, max_length=15, default='eat', verbose_name='Категория')
 
     def __str__(self):
-        return self.name
-
-    # меняет название модели
-    class Meta:
-        verbose_name = "Категория"
-        verbose_name_plural = "Категории"
-
-
-# модель самих товаров с привязкой к классу выше
-class Product(models.Model):
-    name = models.CharField(max_length=200)
-    category = models.ForeignKey(Category, on_delete=models.CASCADE)
-    price = models.DecimalField(max_digits=8, decimal_places=2)
-    composition = models.TextField()
-
-    def __str__(self):
-        return self.name
-
-    class Meta:
-        verbose_name = "Продукт"
-        verbose_name_plural = "Продукты"
+        return self.title
+    
 
--- ScotchProject/scotchapp/views.py
+++ ScotchProject/scotchapp/views.py
@@ -1,10 +1,11 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 from django.conf import settings
+from .models import MenuItem
 
 
 def index(request):
-    context = {
-        'MEDIA_URL': settings.MEDIA_URL,
-    }
+    context = {}
+    for t in MenuItem.TYPE:
+        context[f'menu_{t[0]}'] = MenuItem.objects.filter(type__exact=t[0])
+
     return render(request, 'scotchapp/index.html', context)
